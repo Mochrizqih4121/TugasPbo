81e4800ab3bec3ed35d36a5d0e0069f2d0756d92
MOCH. RIZQI. HP <117371082+Mochrizqih4121@users.noreply.github.com>
2023-10-05 19:10:57 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using HypeBeast;
+
+namespace TokoGaruda71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nikeAirMax = new NikeAirMax97("Nike Air Max 97", 2849000, "42");
+            var nikeDunkPanda = new NikeDunkPanda("Nike Dunk Panda", 1499000, "43");
+            var nikeDunkHighRetro = new NikeDunkHighRetro("Nike Dunk High Retro", 1859000, "44");
+            var jordanFightMVP = new Offwhite("Nike Af1 Mid Pine Green C/0 off-white", 2960000, "41");
+            var chuck70VintageCanvas = new Chuck70VintageCanvas("Chuck 70 Vintage Canvas", 1200000, "39");
+
+            var Tokoku = new Toko("Toko Garuda - 71");
+            Tokoku.TambahProduk(nikeAirMax);
+            Tokoku.TambahProduk(nikeDunkPanda);
+            Tokoku.TambahProduk(nikeDunkHighRetro);
+            Tokoku.TambahProduk(jordanFightMVP);
+            Tokoku.TambahProduk(chuck70VintageCanvas);
+
+            Tokoku.TampilkanProduk();
+
+            Console.Write("Pilih nomor produk yang ingin dibeli: ");
+            int selectedIndex = int.Parse(Console.ReadLine()) - 1;
+            Console.WriteLine("\n");
+
+            if (selectedIndex >= 0 && selectedIndex < Tokoku.Products.Count)
+            {
+                var selectedProduct = Tokoku.Products[selectedIndex];
+                Console.WriteLine(new string('=', 60));
+                Console.WriteLine("Anda telah memilih produk berikut:");
+                Console.WriteLine(new string('=', 60));
+                selectedProduct.Informasi();
+            }
+            else
+            {
+                Console.WriteLine("Produk tidak valid.");
+            }
+        }
+    }
+}
---
+++ Sepatu.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HypeBeast
+{
+    public interface Produk
+    {
+        string Nama { get; set; }
+        double Harga { get; set; }
+        void Informasi();
+    }
+    public abstract class NikeOffWhite : Produk
+    {
+        public string Nama { get; set; }
+        public double Harga { get; set; }
+
+    public NikeOffWhite(string nama, double harga)
+    {
+        Nama = nama;
+        Harga = harga;
+    }
+
+    public abstract void Informasi();
+    }
+
+    public class NikeAirMax97 : NikeOffWhite
+    {
+        public string Ukuran { get; set; }
+
+        public NikeAirMax97(string nama, double harga, string ukuran) : base(nama, harga)
+        {
+            Ukuran = ukuran;
+        }
+
+        public override void Informasi()
+        {
+            Console.WriteLine($"Nama: {Nama}");
+            Console.WriteLine($"Harga: Rp {Harga:N0}");
+            Console.WriteLine($"Ukuran: {Ukuran}");
+        }
+    }
+
+    public class NikeDunkPanda : NikeOffWhite
+    {
+        public string Ukuran { get; set; }
+
+        public NikeDunkPanda(string nama, double harga, string ukuran) : base(nama, harga)
+        {
+            Ukuran = ukuran;
+        }
+
+        public override void Informasi()
+        {
+            Console.WriteLine($"Nama: {Nama}");
+            Console.WriteLine($"Harga: Rp {Harga:N0}");
+            Console.WriteLine($"Ukuran: {Ukuran}");
+        }
+    }
+
+    public class NikeDunkHighRetro : NikeOffWhite
+    {
+        public string Ukuran { get; set; }
+
+        public NikeDunkHighRetro(string nama, double harga, string ukuran) : base(nama, harga)
+        {
+            Ukuran = ukuran;
+        }
+
+        public override void Informasi()
+        {
+            Console.WriteLine($"Nama: {Nama}");
+            Console.WriteLine($"Harga: Rp {Harga:N0}");
+            Console.WriteLine($"Ukuran: {Ukuran}");
+        }
+    }
+
+    public class Offwhite : NikeOffWhite
+    {
+        public string Ukuran { get; set; }
+
+        public Offwhite(string nama, double harga, string ukuran) : base(nama, harga)
+        {
+            Ukuran = ukuran;
+        }
+
+        public override void Informasi()
+        {
+            Console.WriteLine($"Nama: {Nama}");
+            Console.WriteLine($"Harga: Rp {Harga:N0}");
+            Console.WriteLine($"Ukuran: {Ukuran}");
+        }
+    }
+
+    public class Chuck70VintageCanvas : NikeOffWhite
+    {
+        public string Ukuran { get; set; }
+
+        public Chuck70VintageCanvas(string nama, double harga, string ukuran) : base(nama, harga)
+        {
+            Ukuran = ukuran;
+        }
+
+        public override void Informasi()
+        {
+            Console.WriteLine($"Nama: {Nama}");
+            Console.WriteLine($"Harga: Rp {Harga:N0}");
+            Console.WriteLine($"Ukuran: {Ukuran}");
+        }
+    }
+
+    public class Toko
+    {
+        public string Nama { get; set; }
+        public List<NikeOffWhite> Products { get; set; }
+
+        public Toko(string nama)
+        {
+            Nama = nama;
+            Products = new List<NikeOffWhite>();
+        }
+
+        public void TambahProduk(NikeOffWhite product)
+        {
+            Products.Add(product);
+        }
+
+        public void TambahProduk(NikeAirMax97 product)
+        {
+            Products.Add(product);
+        }
+
+        public void TambahProduk(NikeDunkPanda product)
+        {
+            Products.Add(product);
+        }
+
+        public void TambahProduk(NikeDunkHighRetro product)
+        {
+            Products.Add(product);
+        }
+
+        public void TambahProduk(Offwhite product)
+        {
+            Products.Add(product);
+        }
+
+        public void TambahProduk(Chuck70VintageCanvas product)
+        {
+            Products.Add(product);
+        }
+
+        public void TampilkanProduk()
+        {
+            Console.WriteLine(new string('=', 60));
+            Console.WriteLine($"Daftar Produk di {Nama}:");
+            Console.WriteLine(new string('=', 60));
+            for (int i = 0; i < Products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {Products[i].Nama} (Rp {Products[i].Harga:N0})");
+            }
+        }
+    }
+}
